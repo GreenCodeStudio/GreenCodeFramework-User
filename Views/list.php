dbd7d098cb55c2a6c19b1f9e1160932cc975532b
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-07-14 19:51:02 +0200

    datatbles...

--- Views/list.php
+++ Views/list.php
@@ -2,13 +2,15 @@
     <header>
         <h1>Wszyscy użytkownicy</h1>
     </header>
-    <table class="dataTable" data-controller="User" data-method="getTable">
-        <thead>
-        <tr>
-            <th data-value="name">Imie</th>
-            <th data-value="surname">Nazwisko</th>
-            <th data-value="mail">Email</th>
-        </tr>
-        </thead>
-    </table>
+    <div>
+        <table class="dataTable" data-controller="User" data-method="getTable">
+            <thead>
+            <tr>
+                <th data-value="name">Imie</th>
+                <th data-value="surname">Nazwisko</th>
+                <th data-value="mail">Email</th>
+            </tr>
+            </thead>
+        </table>
+    </div>
 </section>
