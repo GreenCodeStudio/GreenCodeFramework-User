b7a8f3880d2edcaefaafe5b2787d210ece660a00
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-07-17 20:04:27 +0200

    akcje datatable

--- Views/list.php
+++ Views/list.php
@@ -2,13 +2,18 @@
     <header>
         <h1>Wszyscy użytkownicy</h1>
     </header>
-    <div>
+    <div class="dataTableContainer">
         <table class="dataTable" data-controller="User" data-method="getTable">
             <thead>
             <tr>
                 <th data-value="name">Imie</th>
                 <th data-value="surname">Nazwisko</th>
                 <th data-value="mail">Email</th>
+                <th class="tableActions">Akcje
+                <div class="tableCopy">
+                    <a href="/User/edit" class="button">Edytuj</a>
+                </div>
+                </th>
             </tr>
             </thead>
         </table>
