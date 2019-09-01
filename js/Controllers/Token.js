156ee25d8920a51b9ce5086d02c3b58edbdffab7
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-09-01 17:02:52 +0200

    refactor

--- js/Controllers/Token.js
+++ js/Controllers/Token.js
@@ -17,19 +17,19 @@ export default class {
         form.loadSelects(this.data.selects);
         form.load(this.data.Token);
 
-        form.submit = async data => {
-            await AjaxTask.startNewTask('Token', 'update', data);
-            pageManager.goto('/Token');
-        }
-    }
+form.submit = async data => {
+    await AjaxTask.startNewTask('Token', 'update', data);
+    pageManager.goto('/Token');
+}
+}
 
-    add() {
-        let form = new FormManager(this.page.querySelector('form'));
-        form.loadSelects(this.data.selects);
+add() {
+    let form = new FormManager(this.page.querySelector('form'));
+    form.loadSelects(this.data.selects);
 
-        form.submit = async data => {
-            await AjaxTask.startNewTask('Token', 'insert', data);
-            pageManager.goto('/Token');
-        }
+    form.submit = async data => {
+        await AjaxTask.startNewTask('Token', 'insert', data);
+        pageManager.goto('/Token');
     }
+}
 }
