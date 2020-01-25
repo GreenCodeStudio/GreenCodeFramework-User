7589d1bfc68fb71998743f622b2e2db304a935f9
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2020-01-25 11:44:52 +0100

    refactor

--- js/Controllers/Token.js
+++ js/Controllers/Token.js
@@ -2,34 +2,31 @@ import {FormManager} from "../../../Core/js/form";
 import {AjaxTask} from "../../../Core/js/ajaxTask";
 import {pageManager} from "../../../Core/js/pageManager";
 
-export default class {
+export class edit {
     constructor(page, data) {
         this.page = page;
         this.data = data;
-    }
-
-    index() {
-
-    }
-
-    edit() {
         let form = new FormManager(this.page.querySelector('form'));
         form.loadSelects(this.data.selects);
         form.load(this.data.Token);
 
-form.submit = async data => {
-    await AjaxTask.startNewTask('Token', 'update', data);
-    pageManager.goto('/Token');
-}
-}
-
-add() {
-    let form = new FormManager(this.page.querySelector('form'));
-    form.loadSelects(this.data.selects);
-
-    form.submit = async data => {
-        await AjaxTask.startNewTask('Token', 'insert', data);
-        pageManager.goto('/Token');
+        form.submit = async data => {
+            await AjaxTask.startNewTask('Token', 'update', data);
+            pageManager.goto('/Token');
+        }
     }
 }
+
+export default class add {
+    constructor(page, data) {
+        this.page = page;
+        this.data = data;
+        let form = new FormManager(this.page.querySelector('form'));
+        form.loadSelects(this.data.selects);
+
+        form.submit = async data => {
+            await AjaxTask.startNewTask('Token', 'insert', data);
+            pageManager.goto('/Token');
+        }
+    }
 }
--- js/Controllers/User.js
+++ js/Controllers/User.js
@@ -2,32 +2,30 @@ import {FormManager} from "../../../Core/js/form";
 import {AjaxTask} from "../../../Core/js/ajaxTask";
 import {pageManager} from "../../../Core/js/pageManager";
 
-export default class {
+export class add {
     constructor(page, data) {
         this.page = page;
         this.data = data;
-        console.log('dd')
+
+        let form = new FormManager(this.page.querySelector('form'));
+
+        form.submit = async data => {
+            await AjaxTask.startNewTask('User', 'insert', data);
+            pageManager.goto('/User');
+        }
     }
+}
 
-    index() {
+export class edit {
+    constructor(page, data) {
+        this.page = page;
+        this.data = data;
 
-    }
-
-    edit() {
         let form = new FormManager(this.page.querySelector('form'));
         form.load(this.data.user);
 
         form.submit = async data => {
             await AjaxTask.startNewTask('User', 'update', data);
-            pageManager.goto('/User');
-        }
-    }
-
-    add() {
-        let form = new FormManager(this.page.querySelector('form'));
-
-        form.submit = async data => {
-            await AjaxTask.startNewTask('User', 'insert', data);
             pageManager.goto('/User');
         }
     }
