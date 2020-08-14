0884d6f7db06858936037550b272c19a82351fe9
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2020-08-14 12:56:05 +0200

    css

--- Views/list.php
+++ Views/list.php
@@ -1,7 +1,7 @@
 <div class="topBarButtons">
     <a href="/User/add" class="button" title="Dodaj"><span class="icon-add"></span>Dodaj</a>
 </div>
-<section class="card" data-width="6">
+<section class="card page-User-list" data-width="6">
     <header>
         <h1>Wszyscy użytkownicy</h1>
     </header>
