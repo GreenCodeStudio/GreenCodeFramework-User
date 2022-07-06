1ed4b2179fe658b691eae9ba2c46f626bf2f3c45
Mateusz Krawczyk <mateusz.krawczyk@green-code.studio>
2022-07-06 14:04:06 +0200

    bugfix

--- Test/Selenium/index.js
+++ Test/Selenium/index.js
@@ -11,8 +11,8 @@ module.exports = class extends BaseSeleniumTest {
         await this.driver.findElement(By.css('a[href="/User"]')).sendKeys(Key.RETURN);
         await this.asleep(1000);
         await this.takeScreenshot('user-before')
-        expect(this.driver.findElement(By.css('.UsersList')).getText()).to.not.contain('TestName')
-        expect(this.driver.findElement(By.css('.UsersList')).getText()).to.not.contain('test@test.example')
+        expect(await this.driver.findElement(By.css('.UsersList')).getText()).to.not.contain('TestName')
+        expect(await this.driver.findElement(By.css('.UsersList')).getText()).to.not.contain('test@test.example')
         await this.driver.findElement(By.css('a[href="/User/add"]')).sendKeys(Key.RETURN);
         await this.asleep(1000);
 
@@ -26,7 +26,7 @@ module.exports = class extends BaseSeleniumTest {
         await this.driver.findElement(By.css('form [name="password2"]')).sendKeys(Key.RETURN);
         await this.asleep(1000);
         await this.takeScreenshot('user-afterAdd')
-        expect(this.driver.findElement(By.css('.UsersList')).getText()).to.contain('TestName')
-        expect(this.driver.findElement(By.css('.UsersList')).getText()).to.contain('test@test.example')
+        expect(await this.driver.findElement(By.css('.UsersList')).getText()).to.contain('TestName')
+        expect(await this.driver.findElement(By.css('.UsersList')).getText()).to.contain('test@test.example')
     }
 }
