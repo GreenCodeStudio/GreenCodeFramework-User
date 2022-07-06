327edfe8a5ac902646f059e8f6dfa70affb62e05
Mateusz Krawczyk <mateusz.krawczyk@green-code.studio>
2022-07-06 13:42:13 +0200

    tests

--- Test/Selenium/index.js
+++ Test/Selenium/index.js
@@ -10,6 +10,8 @@ module.exports = class extends BaseSeleniumTest {
         await this.driver.findElement(By.css('a[href="/User"]')).sendKeys(Key.RETURN);
         await this.asleep(1000);
         await this.takeScreenshot('user-before')
+        expect(this.driver.findElement(By.css('.UsersList')).getText()).to.not.contain('TestName')
+        expect(this.driver.findElement(By.css('.UsersList')).getText()).to.not.contain('test@test.example')
         await this.driver.findElement(By.css('a[href="/User/add"]')).sendKeys(Key.RETURN);
         await this.asleep(1000);
 
@@ -19,9 +21,11 @@ module.exports = class extends BaseSeleniumTest {
         await this.driver.findElement(By.css('form [name="mail"]')).sendKeys('test@test.example');
         await this.driver.findElement(By.css('form [name="password"]')).sendKeys('pass@!12');
         await this.driver.findElement(By.css('form [name="password2"]')).sendKeys('pass@!12');
-        await this.takeScreenshot('user-add-filled')
+        await this.takeScreenshot('user-add-filled', true)
         await this.driver.findElement(By.css('form [name="password2"]')).sendKeys(Key.RETURN);
         await this.asleep(1000);
         await this.takeScreenshot('user-afterAdd')
+        expect(this.driver.findElement(By.css('.UsersList')).getText()).to.contain('TestName')
+        expect(this.driver.findElement(By.css('.UsersList')).getText()).to.contain('test@test.example')
     }
 }
