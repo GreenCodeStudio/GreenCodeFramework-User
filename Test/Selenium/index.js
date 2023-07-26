0defffd793e6a2cf7cb9f687fe5d74d67655ad30
PiotrKukula0 <piotr.kukula@green-code.studio>
2023-07-26 11:44:05 +0200

    Fix tests

--- Test/Selenium/index.js
+++ Test/Selenium/index.js
@@ -35,6 +35,12 @@ module.exports = class extends BaseSeleniumTest {
         await this.sendKeysToElement('.card [name="mass"]', "3");
         await this.sendKeysToElement('.card [name="unitType"]', "4");
         await this.clickElement('.icon-save');
+        await this.clickElement('.icon-add');
+        await this.sendKeysToElement('.card [name="name"]', "5");
+        await this.sendKeysToElement('.card [name="code"]', "6");
+        await this.sendKeysToElement('.card [name="mass"]', "7");
+        await this.sendKeysToElement('.card [name="unitType"]', "8");
+        await this.clickElement('.icon-save');
         await this.asleep(1000);
         await this.takeScreenshot('WarehouseArticle');
 
@@ -43,24 +49,31 @@ module.exports = class extends BaseSeleniumTest {
         await this.sendKeysToElement('.card [name="name"]', "test");
         await this.sendKeysToElement('.addNext', "1");
         await this.clickElement('.icon-save');
+        await this.clickElement('.icon-add');
+        await this.sendKeysToElement('.card [name="name"]', "test2");
+        await this.sendKeysToElement('.addNext', "2");
+        await this.clickElement('.icon-save');
         await this.asleep(1000);
         await this.takeScreenshot('Receipt');
 
         await this.openURL('/Picking');
         await this.clickElement('.icon-add');
         await this.sendKeysToElement('.card [name="name"]', "test");
-        await this.sendKeysToElement('.addNext', "1");
+        await this.sendKeysToElement('.addNext', "2");
+        await this.clickElement('.icon-save');
+        await this.clickElement('.icon-add');
+        await this.sendKeysToElement('.card [name="name"]', "test2");
+        await this.sendKeysToElement('.addNext', "6");
         await this.clickElement('.icon-save');
         await this.asleep(1000);
         await this.takeScreenshot('Picking');
-        await this.clickElement('.icon-edit');
-        await this.sendKeysToElement('.addNext', "2");
-        await this.clickElement('.icon-save');
 
         await this.openURL('/Receipt');
         await this.clickElement('.icon-edit');
         await this.sendKeysToElement('.addNext', "2");
         await this.clickElement('button.button[value="confirm"]');
+        await this.asleep(1000);
+        await this.takeScreenshot('Receipt-save');
         // expect(await this.driver.findElement(By.css('.UsersList')).getText()).to.contain('TestName')
         // expect(await this.driver.findElement(By.css('.UsersList')).getText()).to.contain('test@test.example')
     }
