85018ac87c56a98d9dc2f8cfaa0bbdba14fe2fc8
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-06-01 16:18:51 +0200

    Extracted from user module

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
 import {pageManager} from "../../Core/js/pageManager";
 
 pageManager.registerController('User', () => import('./Controllers/User'));
-pageManager.registerController('Token', () => import('./Controllers/Token'));
